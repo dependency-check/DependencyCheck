f9fcc259c34ed4c58dd8f83bf274a68843fbc7c7
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2025-09-21 15:55:27 +0800

    fix: Correct CVSSv4 parsing for low precision OSSIndex values
    Signed-off-by: Chad Wilson <29788154+chadlwilson@users.noreply.github.com>

--- core/src/test/java/org/owasp/dependencycheck/utils/CvssUtilTest.java
+++ core/src/test/java/org/owasp/dependencycheck/utils/CvssUtilTest.java
@@ -21,10 +21,13 @@ import io.github.jeremylong.openvulnerability.client.nvd.CvssV2;
 import io.github.jeremylong.openvulnerability.client.nvd.CvssV2Data;
 import io.github.jeremylong.openvulnerability.client.nvd.CvssV3;
 import io.github.jeremylong.openvulnerability.client.nvd.CvssV3Data;
+import io.github.jeremylong.openvulnerability.client.nvd.CvssV4;
+import io.github.jeremylong.openvulnerability.client.nvd.CvssV4Data;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  *
@@ -58,10 +61,12 @@ class CvssUtilTest {
     void testCvssV2ScoreToSeverity() {
         assertEquals("UNKNOWN", CvssUtil.cvssV2ScoreToSeverity(-1.0));
         assertEquals("LOW", CvssUtil.cvssV2ScoreToSeverity(0.0));
+        assertEquals("LOW", CvssUtil.cvssV2ScoreToSeverity(0.05));
         assertEquals("LOW", CvssUtil.cvssV2ScoreToSeverity(1.0));
         assertEquals("LOW", CvssUtil.cvssV2ScoreToSeverity(3.9));
         assertEquals("MEDIUM", CvssUtil.cvssV2ScoreToSeverity(4.0));
         assertEquals("MEDIUM", CvssUtil.cvssV2ScoreToSeverity(6.9));
+        assertEquals("MEDIUM", CvssUtil.cvssV2ScoreToSeverity((double) 6.9f)); // test low-precision floating point values
         assertEquals("HIGH", CvssUtil.cvssV2ScoreToSeverity(7.0));
         assertEquals("HIGH", CvssUtil.cvssV2ScoreToSeverity(10.0));
         assertEquals("UNKNOWN", CvssUtil.cvssV2ScoreToSeverity(11.0));
@@ -73,10 +78,12 @@ class CvssUtilTest {
     @Test
     void testCvssV3ScoreToSeverity() {
         assertEquals(CvssV3Data.SeverityType.NONE, CvssUtil.cvssV3ScoreToSeverity(0.0));
+        assertEquals(CvssV3Data.SeverityType.LOW, CvssUtil.cvssV3ScoreToSeverity(0.05));
         assertEquals(CvssV3Data.SeverityType.LOW, CvssUtil.cvssV3ScoreToSeverity(1.0));
         assertEquals(CvssV3Data.SeverityType.LOW, CvssUtil.cvssV3ScoreToSeverity(3.9));
         assertEquals(CvssV3Data.SeverityType.MEDIUM, CvssUtil.cvssV3ScoreToSeverity(4.0));
         assertEquals(CvssV3Data.SeverityType.MEDIUM, CvssUtil.cvssV3ScoreToSeverity(6.9));
+        assertEquals(CvssV3Data.SeverityType.MEDIUM, CvssUtil.cvssV3ScoreToSeverity((double) 6.9f)); // test low-precision floating point values
         assertEquals(CvssV3Data.SeverityType.HIGH, CvssUtil.cvssV3ScoreToSeverity(7.0));
         assertEquals(CvssV3Data.SeverityType.HIGH, CvssUtil.cvssV3ScoreToSeverity(8.9));
         assertEquals(CvssV3Data.SeverityType.CRITICAL, CvssUtil.cvssV3ScoreToSeverity(9.0));
@@ -104,6 +111,54 @@ class CvssUtilTest {
         assertEquals(CvssV3Data.CiaType.HIGH, result.getCvssData().getAvailabilityImpact());
         assertEquals(CvssV3Data.SeverityType.CRITICAL, result.getCvssData().getBaseSeverity());
         assertEquals(10.0, result.getCvssData().getBaseScore(), 0);
+    }
+
+    /**
+     * Test of cvssV4ScoreToSeverity method, of class CvssUtil.
+     */
+    @Test
+    void testCvssV4ScoreToSeverity() {
+        assertEquals(CvssV4Data.SeverityType.NONE, CvssUtil.cvssV4ScoreToSeverity(0.0));
+        assertEquals(CvssV4Data.SeverityType.LOW, CvssUtil.cvssV4ScoreToSeverity(0.05));
+        assertEquals(CvssV4Data.SeverityType.LOW, CvssUtil.cvssV4ScoreToSeverity(1.0));
+        assertEquals(CvssV4Data.SeverityType.LOW, CvssUtil.cvssV4ScoreToSeverity(3.9));
+        assertEquals(CvssV4Data.SeverityType.MEDIUM, CvssUtil.cvssV4ScoreToSeverity(4.0));
+        assertEquals(CvssV4Data.SeverityType.MEDIUM, CvssUtil.cvssV4ScoreToSeverity(6.9));
+        assertEquals(CvssV4Data.SeverityType.MEDIUM, CvssUtil.cvssV4ScoreToSeverity(6.9f)); // test low-precision floating point values
+        assertEquals(CvssV4Data.SeverityType.HIGH, CvssUtil.cvssV4ScoreToSeverity(7.0));
+        assertEquals(CvssV4Data.SeverityType.HIGH, CvssUtil.cvssV4ScoreToSeverity(8.9));
+        assertEquals(CvssV4Data.SeverityType.CRITICAL, CvssUtil.cvssV4ScoreToSeverity(9.0));
+        assertEquals(CvssV4Data.SeverityType.CRITICAL, CvssUtil.cvssV4ScoreToSeverity(10.0));
+        assertThrows(IllegalArgumentException.class, () -> CvssUtil.cvssV4ScoreToSeverity(11.0));
+        assertThrows(IllegalArgumentException.class, () -> CvssUtil.cvssV4ScoreToSeverity(-1.0));
+    }
+
+    /**
+     * Test of vectorToCvssV4 method, of class CvssUtil.
+     */
+    @Test
+    void testVectorToCvssV4() {
+        String vectorString = "CVSS:4.0/AV:N/AC:L/AT:P/PR:N/UI:N/VC:H/VI:N/VA:N/SC:N/SI:N/SA:N";
+        Double baseScore = 8.2;
+        String source = "ossIndex";
+        CvssV4.Type type = CvssV4.Type.PRIMARY;
+        CvssV4 result = CvssUtil.vectorToCvssV4(source, type, baseScore, vectorString);
+        assertEquals(CvssV4Data.Version._4_0, result.getCvssData().getVersion());
+        assertEquals(source, result.getSource());
+        assertEquals(type, result.getType());
+        assertEquals(CvssV4Data.AttackVectorType.NETWORK, result.getCvssData().getAttackVector());
+        assertEquals(CvssV4Data.AttackComplexityType.LOW, result.getCvssData().getAttackComplexity());
+        assertEquals(CvssV4Data.AttackRequirementsType.PRESENT, result.getCvssData().getAttackRequirements());
+        assertEquals(CvssV4Data.PrivilegesRequiredType.NONE, result.getCvssData().getPrivilegesRequired());
+        assertEquals(CvssV4Data.UserInteractionType.NONE, result.getCvssData().getUserInteraction());
+        assertEquals(CvssV4Data.CiaType.HIGH, result.getCvssData().getVulnConfidentialityImpact());
+        assertEquals(CvssV4Data.CiaType.NONE, result.getCvssData().getVulnIntegrityImpact());
+        assertEquals(CvssV4Data.CiaType.NONE, result.getCvssData().getVulnAvailabilityImpact());
+        assertEquals(CvssV4Data.CiaType.NONE, result.getCvssData().getSubConfidentialityImpact());
+        assertEquals(CvssV4Data.CiaType.NONE, result.getCvssData().getSubIntegrityImpact());
+        assertEquals(CvssV4Data.CiaType.NONE, result.getCvssData().getSubAvailabilityImpact());
+        assertEquals(CvssV4Data.SeverityType.HIGH, result.getCvssData().getBaseSeverity());
+        assertEquals(8.2, result.getCvssData().getBaseScore(), 0);
     }
 
 }
