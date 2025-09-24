ccec0f7ea5bf3048a96e67b357fbcdf8bfe7cc91
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2025-09-24 23:29:56 +0800

    build: Exit and print error if docker is not running with the containerd snapshotter
    This is currently required to do a `docker buildx build --load` alongside multi-platform build, due to needing a convenient workaround for https://github.com/docker/buildx/issues/59#issuecomment-2612971318
    
    Signed-off-by: Chad Wilson <29788154+chadlwilson@users.noreply.github.com>

--- build-docker.sh
+++ build-docker.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -e
+#!/bin/bash
+set -euo pipefail
 
 VERSION=$(mvn -q \
     -Dexec.executable="echo" \
@@ -7,13 +8,20 @@ VERSION=$(mvn -q \
     org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
 
 FILE=./cli/target/dependency-check-$VERSION-release.zip
-if [ -f "$FILE" ]; then
-    extra_tag_args="$([[ ! $VERSION = *"SNAPSHOT"* ]] && echo "--tag owasp/dependency-check:latest" || echo "")"
-
-    docker buildx build --pull --load --platform linux/amd64,linux/arm64 . \
-      --build-arg VERSION=$VERSION \
-      --tag owasp/dependency-check:$VERSION ${extra_tag_args}
-else 
+if [ ! -f "$FILE" ]; then
     echo "$FILE does not exist - run 'mvn package' first"
     exit 1
 fi
+
+if ! docker info -f '{{ .DriverStatus }}' | grep "driver-type io.containerd.snapshotter" >/dev/null; then
+    echo "Docker Engine is not running with the containerd snapshotter - this is currently needed to build and test ODC multi-platform images using docker buildx."
+    echo "If using Docker Desktop, enable \"Use containerd for pulling and storing images\" per https://docs.docker.com/desktop/settings-and-maintenance/settings/#general"
+    echo "For more technical information on Docker Engine, see https://docs.docker.com/engine/storage/containerd/"
+    exit 1
+fi
+
+extra_tag_args="$([[ ! $VERSION = *"SNAPSHOT"* ]] && echo "--tag owasp/dependency-check:latest" || echo "")"
+
+docker buildx build --pull --load --platform linux/amd64,linux/arm64 . \
+    --build-arg VERSION=$VERSION \
+    --tag owasp/dependency-check:$VERSION ${extra_tag_args}
