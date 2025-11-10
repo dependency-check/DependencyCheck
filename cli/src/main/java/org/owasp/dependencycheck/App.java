9eca5b4c526cb095edea9dabe4917c61711920a8
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2025-11-10 13:16:13 +0800

    fix: make JCS logging adapter configuration self-contained with DataCacheFactory
    - allows us to workaround #8108
    - makes API for `core` consistently SLF4J based
    - debug logging derived from logger configuration for the DataCacheFactory itself
    
    Signed-off-by: Chad Wilson <29788154+chadlwilson@users.noreply.github.com>

--- cli/src/main/java/org/owasp/dependencycheck/App.java
+++ cli/src/main/java/org/owasp/dependencycheck/App.java
@@ -48,7 +48,6 @@ import ch.qos.logback.classic.filter.ThresholdFilter;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
-import io.github.jeremylong.jcs3.slf4j.Slf4jAdapter;
 import java.util.TreeSet;
 import org.owasp.dependencycheck.utils.SeverityUtil;
 
@@ -84,10 +83,6 @@ public class App {
      */
     @SuppressWarnings("squid:S4823")
     public static void main(String[] args) {
-        System.setProperty("jcs.logSystem", "slf4j");
-        if (!LOGGER.isDebugEnabled()) {
-            Slf4jAdapter.muteLogging(true);
-        }
         final int exitCode;
         final App app = new App();
         exitCode = app.run(args);
