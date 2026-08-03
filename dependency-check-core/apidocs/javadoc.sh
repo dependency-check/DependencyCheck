7f3556bd9ac04dd320806618c0d4608de4ddb971
jeremylong <jeremy.long@gmail.com>
2026-08-03 10:42:45 +0000

    Deploying to gh-pages from @ dependency-check/DependencyCheck@4aacdb10707fe88a55b873dd8f7235ad4ad3555f 🚀

--- dependency-check-core/apidocs/javadoc.sh
+++ dependency-check-core/apidocs/javadoc.sh
@@ -1,1 +1,1 @@
-/opt/hostedtoolcache/Java_Zulu_jdk/25.0.3-9/x64/bin/javadoc -J-Duser.language= -J-Duser.country= @options @argfile
+/opt/hostedtoolcache/Java_Zulu_jdk/25.0.4-7/x64/bin/javadoc -J-Duser.language= -J-Duser.country= @options @argfile
